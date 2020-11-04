e41ef100a598fa1aa26372e01604e6fe89413730
DH43EFd <pieterjan.de.muinck@ing.com>
2020-11-04 23:59:34 +0100

    show all bookmaker odds

--- frontend/app/odds-comparison/src/modules/betoffers/betoffers.js
+++ frontend/app/odds-comparison/src/modules/betoffers/betoffers.js
@@ -3,24 +3,54 @@ import api from './api'
 import useInterval from '../utils/userinterval'
 import Prices from './prices'
 import BetOptions from './betoptions'
+import Bookmaker from './bookmaker'
 
 function BetOffers({event}) {
 
-    const [betOffers, setBetOffers] = React.useState()
+    const [kambi, setKambi] = React.useState()
+    const [sbtech, setSbtech] = React.useState()
+    const [pinnacle, setPinnacle] = React.useState()
 
     useInterval(() => {
         async function getBetOffers() {
-            const betOffers = api.getBetOffers().filter(betOffer => betOffer.eventId == event.pinnacleEventId)
-            setBetOffers(betOffers)
+            const betOffers = api.getBetOffers()
+            if(event.kambiEvent) {
+                setKambi(betOffers.filter(betOffer => betOffer.eventId == event.kambiEvent.id))
+            } 
+            if(event.sbtechEvent) {
+                setSbtech(betOffers.filter(betOffer => betOffer.eventId == event.sbtechEvent.id))
+            } 
+            if(event.pinnacleEventId) {
+                setPinnacle(betOffers.filter(betOffer => betOffer.eventId == event.pinnacleEventId))
+            }
         }
         getBetOffers()
     }, 60*100)
 
     return (
         <div>
-            {betOffers && betOffers.map(betOffer => {
+            {kambi && kambi.map(betOffer => {
                 return(
                     <div>
+                        <Bookmaker book={betOffer.book}></Bookmaker>
+                        <BetOptions betOffer={betOffer}></BetOptions>
+                        <Prices betOffer={betOffer}></Prices>
+                    </div>
+                )  
+            })}
+            {sbtech && sbtech.map(betOffer => {
+                return(
+                    <div>
+                        <Bookmaker book={betOffer.book}></Bookmaker>
+                        <BetOptions betOffer={betOffer}></BetOptions>
+                        <Prices betOffer={betOffer}></Prices>
+                    </div>
+                )  
+            })}
+            {pinnacle && pinnacle.map(betOffer => {
+                return(
+                    <div>
+                        <Bookmaker book={betOffer.book}></Bookmaker>
                         <BetOptions betOffer={betOffer}></BetOptions>
                         <Prices betOffer={betOffer}></Prices>
                     </div>
