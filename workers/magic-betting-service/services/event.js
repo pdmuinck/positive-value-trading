a1fda5a78ab1f66f132c01761f6c373475d22e67
DH43EFd <pieterjan.de.muinck@ing.com>
2020-11-29 13:19:38 +0100

    improve retrieval magi

--- workers/magic-betting-service/services/event.js
+++ workers/magic-betting-service/services/event.js
@@ -5,8 +5,6 @@ const cache = new NodeCache({ stdTTL: ttlSeconds, checkperiod: ttlSeconds * 0.2,
 const CDP = require('chrome-remote-interface')
 const chromeLauncher = require('chrome-launcher');
 
-
-
 async function launchChrome() {
     return await chromeLauncher.launch({
     chromeFlags: [
@@ -18,22 +16,16 @@ async function launchChrome() {
     })
 }
 
-
-
-
-//let circusWS = new WebSocket("wss://magicbetting.be/api/234/nmbzjqkk/websocket", null, {rejectUnauthorized: false})
-
-
-
 const event = {}
 
 event.getBySport = async (sport) => {
-
     let circusWS = new WebSocket(cache.get('API-URL'), null, {rejectUnauthorized: false})
+
+    
 
 
     circusWS.on('open', function open() {
-        circusWS.send(JSON.stringify(["CONNECT\nprotocol-version:1.5\naccept-version:1.1,1.0\nheart-beat:10000,10000\n\n\u0000"]))
+        //circusWS.send(JSON.stringify(["CONNECT\nprotocol-version:1.5\naccept-version:1.1,1.0\nheart-beat:10000,10000\n\n\u0000"]))
         /*
             circusWS.send(JSON.stringify(["SUBSCRIBE\nid:/user/request-response\ndestination:/user/request-response\n\n\u0000"]))
         circusWS.send(JSON.stringify(["SUBSCRIBE\nid:/user/error\ndestination:/user/error\n\n\u0000"]))
@@ -52,7 +44,7 @@ event.getBySport = async (sport) => {
         
         //circusWS.send(JSON.stringify(["SUBSCRIBE\nid:/api/sports/soccer\ndestination:/api/sports/soccer\nlocale:nl\n\n\u0000"]))
         //circusWS.send(JSON.stringify(["SUBSCRIBE\nid:/api/eventgroups/soccer-live-match-events-grouped-by-type\ndestination:/api/eventgroups/soccer-live-match-events-grouped-by-type\nlocale:nl\n\n\u0000"]))
-        circusWS.send(JSON.stringify(["SUBSCRIBE\nid:/api/eventgroups/soccer-int10-sb_type_19162-all-match-events-grouped-by-type\ndestination:/api/eventgroups/soccer-int10-sb_type_19162-all-match-events-grouped-by-type\nlocale:nl\n\n\u0000"]))
+        circusWS.send(JSON.stringify(["SUBSCRIBE\nid:/api/eventgroups/soccer-all-match-events-grouped-by-type\ndestination:/api/eventgroups/soccer-all-match-events-grouped-by-type\nlocale:nl\n\n\u0000"]))
         //circusWS.send(JSON.stringify(["SUBSCRIBE\nid:/api/events/3243155496\ndestination:/api/events/3243155496\nlocale:nl\n\n\u0000"]))
         
 
@@ -62,14 +54,25 @@ event.getBySport = async (sport) => {
     })
 
     circusWS.on('message', function incoming(data) {
-        console.log(data.substring(1))
+        if(data.includes('soccer-all-match-events')) {
+            const test = data.substring(12)
+            const test2 = test.substring(0, test.length - 1)
+            const test3 = test2.substring(test2.indexOf('{'))
+            cache.set('test', test3.replace('\\\\\\', ''))
+        }
+        
+        //JSON.parse(JSON.stringify(data.substring(1)))
         //cache.set('FOOTBALL', data)
     })
+
+    return cache.get('test')
 
 }
 
 setInterval(async () => {
-    const chrome = await launchChrome();
+    await chromeLauncher.killAll()
+    const chrome = await launchChrome()
+    
     const protocol = await CDP({
       port: chrome.port
     });
---
+++ workers/magic-betting-service/test.js
@@ -0,0 +1,3 @@
+const data = require('./test.json')
+
+console.log(data)
