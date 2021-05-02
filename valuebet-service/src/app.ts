99f3906a5167d6ba13c6e30ce26c444914f302a2
pieterjandemuinck <pieterjan.demuinck@persgroep.net>
2021-05-02 16:15:38 +0200

    setup lambda

---
+++ valuebet-service/src/app.ts
@@ -0,0 +1,9 @@
+import {EventInfo} from "./service/events";
+import {Scraper} from "./client/scraper";
+
+exports.handler = async function getEvents() {
+    const events: EventInfo[] = await Scraper.getEventsForLeague()
+}
+
+
+
--- valuebet-service/src/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var endpoints = require('./setup/endpoints');
-var middlewares = require('./setup/middlewares');
-var start = require('./setup/start');
-var server = express();
-middlewares(server);
-endpoints(server);
-start(server);
