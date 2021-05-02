5f809aac98be59e64131523af265194cf960885a
pieterjandemuinck <pieterjan.demuinck@persgroep.net>
2021-05-02 20:32:05 +0200

    attempt dockerfile and make file

--- valuebet-service/src/event-mapper/app.ts
+++ valuebet-service/src/event-mapper/app.ts
@@ -1,5 +1,5 @@
-import {EventInfo} from "./service/events";
-import {Scraper} from "./client/scraper";
+import {EventInfo} from "../service/events";
+import {Scraper} from "../client/scraper";
 
 exports.handler = async function getEvents() {
     const events: EventInfo[] = await Scraper.getEventsForLeague()
