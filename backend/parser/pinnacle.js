124269d9e35faa8e226c23fbfcba3951b33e9499
pieterjandemuinck <pieterjan.demuinck@persgroep.net>
2021-06-02 22:36:48 +0200

    fix pinnacle parser

--- backend/parser/pinnacle.js
+++ backend/parser/pinnacle.js
@@ -1,48 +1,44 @@
+const {sortBetOffers} = require("../utils");
 const {Bookmaker, BetType} = require("./bookmaker")
 const {calculateMargin, BetOffer} = require("../utils")
 
 
-exports.parsePinnacleBetOffers = function parsePinnacleBetOffers(apiResponse) {
-    let betOffers = []
-    for(let i = 0; i < apiResponse.data.length; i +=2) {
-        const test = parseBetOffers(apiResponse.data[i], apiResponse.data[i+1])
-        betOffers = betOffers.concat(test)
-    }
-    return betOffers
-}
-
-function parseBetOffers(marketIds, offers) {
-    const betOffers = []
-    marketIds.forEach(marketId => {
-        const marketOffers = offers.filter(offer => offer.matchupId === marketId.id).flat()
+exports.parsePinnacleBetOffers = function parsePinnacleBetOffers(markets, betOffers) {
+    let parsedBetOffers = []
+    markets.forEach(market => {
+        const marketOffers = betOffers.filter(offer => offer.matchupId === market.id).flat()
         marketOffers.forEach(offer => {
-            const betType = determineBetType(offer.key, marketId)
+            const betType = determineBetType(offer.key, market)
             if(betType !== BetType.UNKNOWN) {
                 const margin = calculateMargin(offer.prices.map(price => toDecimalOdds(price.price)))
                 offer.prices.forEach(price => {
-                    const outcome = determineOutcome(price.designation, marketId, price.participantId, betType)
-                    const line = price.points ? price.points : undefined
+                    const outcome = determineOutcome(price.designation, market, price.participantId, betType)
+                    const line = determineLine(betType, market, price.points)
                     const odds = toDecimalOdds(price.price)
-
-                    betOffers.push(new BetOffer(betType, marketId.parent ? marketId.parent.id : marketId.id, Bookmaker.PINNACLE, outcome, odds, line, margin))
+                    if(betType === BetType.OVER_UNDER && !line) return
+                    parsedBetOffers.push(new BetOffer(betType, market.parent ? market.parent.id : market.id, Bookmaker.PINNACLE, outcome, odds, line, margin))
                 })
             }
         })
     })
-    return betOffers
+    return parsedBetOffers.flat().filter(x => x).sort(sortBetOffers).map(betOffer => {
+        betOffer.betType = betOffer.betType.name
+        return betOffer
+    })
 }
 
-function determineBetType(key, marketId) {
+function determineBetType(key, market) {
     const splitted = key.split(";")
     const period = splitted[1]
     const type = splitted[2]
-    if(!marketId.parent) {
+    if(!market.parent) {
         // primary markets
         if(type === "m") {
             if(period === "0") return BetType._1X2
             if(period === "1") return BetType._1X2_H1
         }
         if(type === "tt"){
+            /*
             if(splitted[4] === "home") {
                 if(period === "0") return BetType.OVER_UNDER_TEAM1
                 if(period === "1") return BetType.OVER_UNDER_TEAM1_H1
@@ -51,23 +47,26 @@ function determineBetType(key, marketId) {
                 if(period === "0") return BetType.OVER_UNDER_TEAM2
                 if(period === "1") return BetType.OVER_UNDER_TEAM2_H1
             }
+
+             */
         }
+        /*
         if(type === "s") {
             if(period === "0") return BetType.ASIAN_HANDICAP
             if(period === "1") return BetType.ASIAN_HANDICAP_H1
         }
 
+         */
+
         if(type === "ou") {
             if(period === "0") return BetType.OVER_UNDER
-            if(period === "1") return BetType.OVER_UNDER_H1
+            // if(period === "1") return BetType.OVER_UNDER_H1
         }
-    } else if(marketId.type === "special") {
-        const homeTeam = marketId.parent.participants.filter(p => p.alignment === "home").map(p => p.name)[0]
-        const awayTeam = marketId.parent.participants.filter(p => p.alignment === "away").map(p => p.name)[0]
-        if(marketId.special.description.includes("3-way Handicap")) return BetType.HANDICAP
+    } else if(market.type === "special") {
+        if(market.special.description.includes("3-way Handicap")) return BetType.HANDICAP
 
         // specials
-        switch(marketId.special.description) {
+        switch(market.special.description) {
             case "Double Chance 1st Half":
                 return BetType.DOUBLE_CHANCE_H1
             case "Double Chance":
@@ -80,19 +79,24 @@ function determineBetType(key, marketId) {
                 return BetType.BOTH_TEAMS_SCORE_H1
             case "Draw No Bet 1st Half":
                 return BetType.DRAW_NO_BET_H1
+            /*
             case "Exact Total Goals":
                 return BetType.EXACT_GOALS
+
+             */
             case "Draw No Bet":
                 return BetType.DRAW_NO_BET
             case "Total Goals Odd/Even":
                 return BetType.ODD_EVEN
-            case "Total Goals Odd/Even 1st Half":
-                return BetType.ODD_EVEN_H1
+            /*
             case "Correct Score":
                 return BetType.CORRECT_SCORE
 
+             */
+
         }
 
+        /*
         if(marketId.special.description === awayTeam + " Goals Odd/Even") return BetType.ODD_EVEN_TEAM2
         if(marketId.special.description === homeTeam + " Goals Odd/Even") return BetType.ODD_EVEN_TEAM1
         if(marketId.special.description === homeTeam + " Goals 1st Half") return BetType.TOTAL_GOALS_TEAM1_H1
@@ -100,12 +104,14 @@ function determineBetType(key, marketId) {
         if(marketId.special.description === homeTeam + " Goals") return BetType.TOTAL_GOALS_TEAM1
         if(marketId.special.description === awayTeam + " Goals") return BetType.TOTAL_GOALS_TEAM2
 
-    }
+         */
 
-    if(splitted)
-        if(key === 's;0;m') return BetType._1X2
+    }
+    /*
     if(key.includes('s;0;ou')) return BetType.ASIAN_OVER_UNDER
     if(key.includes("s;0;s")) return BetType.ASIAN_HANDICAP
+
+     */
     if(key.includes(""))
         return BetType.UNKNOWN
 }
@@ -120,11 +126,41 @@ function toDecimalOdds(americanOdds) {
 
 }
 
-function determineOutcome(outcome, marketId, participantId, betType) {
-    if(betType === BetType.CORRECT_SCORE || betType === BetType.CORRECT_SCORE_H1) {
-        return marketId.participants.filter(participant => participant.id === participantId)[0].name.split(",").map(o => o.split(" ")).map(test => test[test.length - 1]).join("-")
+function determineLine(betType, market, points) {
+    if(betType === BetType.HANDICAP) {
+        if(market.special.description.includes("-5")) return "0:5"
+        if(market.special.description.includes("-4")) return "0:4"
+        if(market.special.description.includes("-3")) return "0:3"
+        if(market.special.description.includes("-2")) return "0:2"
+        if(market.special.description.includes("-1")) return "0:1"
+        if(market.special.description.includes("+5")) return "5:0"
+        if(market.special.description.includes("+4")) return "4:0"
+        if(market.special.description.includes("+3")) return "3:0"
+        if(market.special.description.includes("+2")) return "2:0"
+        if(market.special.description.includes("+1")) return "1:0"
     }
-    if(!marketId.parent) {
+    if(betType === BetType.OVER_UNDER) {
+        if(points === 0.5) return "0.5"
+        if(points === 1.5) return "1.5"
+        if(points === 2.5) return "2.5"
+        if(points === 3.5) return "3.5"
+        if(points === 4.5) return "4.5"
+        if(points === 5.5) return "5.5"
+        return null
+    }
+
+    return points ? points : null
+
+}
+
+function determineOutcome(outcome, market, participantId, betType) {
+    const homeTeam = market.parent ? market.parent.participants.filter(p => p.alignment === "home").map(p => p.name)[0].toUpperCase() : null
+    const awayTeam = market.parent ? market.parent.participants.filter(p => p.alignment === "away").map(p => p.name)[0].toUpperCase() : null
+    if(betType === BetType.CORRECT_SCORE || betType === BetType.CORRECT_SCORE_H1) {
+        return market.participants.filter(participant => participant.id === participantId)[0].name.split(",").map(o => o.split(" ")).map(test => test[test.length - 1]).join("-")
+    }
+
+    if(!market.parent) {
         switch(outcome.toUpperCase()) {
             case 'HOME':
                 return '1'
@@ -136,7 +172,23 @@ function determineOutcome(outcome, marketId, participantId, betType) {
                 return outcome.toUpperCase()
         }
     } else {
-        return marketId.participants.filter(participant => participant.id === participantId)[0].name.toUpperCase()
+        let outcome = market.participants.filter(participant => participant.id === participantId)[0].name.toUpperCase()
+        if(betType === BetType.DOUBLE_CHANCE || betType === BetType.DOUBLE_CHANCE_H1) {
+            if(outcome.includes("OR DRAW")) return "1X"
+            if(outcome.includes("DRAW OR")) return "X2"
+            return "12"
+        }
+        if(betType === BetType.DRAW_NO_BET || betType === BetType.DRAW_NO_BET_H1) {
+            if(outcome === homeTeam) return "1"
+            return "2"
+        }
+        if(betType === BetType.HANDICAP) {
+            if(outcome.includes("DRAW")) return "X"
+            if(outcome.includes(homeTeam)) return "1"
+            return "2"
+        }
+        return outcome
+
     }
 
 }
--- backend/test/parser_test.js
+++ backend/test/parser_test.js
@@ -1,8 +1,7 @@
-
-
 const {parseAltenarBetOffers} = require("../parser/altenar")
 const {parseBetwayBetOffers} = require("../parser/betway")
 const assert = require("assert")
+const {parsePinnacleBetOffers} = require("../parser/pinnacle");
 const {parserMeridianBetOffers} = require("../parser/meridian");
 const {parseLadbrokesBetOffers} = require("../parser/ladbrokes");
 const {parseKambiBetOffers} = require("../parser/kambi");
@@ -12,7 +11,8 @@ const {parseBingoalBetOffers} = require("../parser/bingoal");
 const {describe} = require("mocha")
 
 describe("Parsers", function() {
-    const books = [{name: "meridian", parser: parserMeridianBetOffers}]
+    const books = []
+    //const books = [{name: "pinnacle", parser: parsePinnacleBetOffers}]
     books.forEach(book => {
         it("check " + book, function() {
             const betOffers = require("../test/resources/" + book.name + "/betoffers.json")
@@ -25,6 +25,19 @@ describe("Parsers", function() {
             //assert.deepStrictEqual(JSON.stringify(result), JSON.stringify(expected))
             assert.deepStrictEqual(result, expected)
         })
+    })
+
+    it("check pinnacle", function() {
+        const betTypes = require("./resources/pinnacle/markets.json")
+        const betOffers = require("../test/resources/pinnacle/betoffers.json")
+        const expected = require("../test/resources/pinnacle/expected_betoffers.json")
+        const result = parsePinnacleBetOffers(betTypes, betOffers.data)
+        result.forEach(result => {
+            delete result.margin
+            delete result.key
+        })
+        //assert.deepStrictEqual(JSON.stringify(result), JSON.stringify(expected))
+        assert.deepStrictEqual(result, expected)
     })
 
 })
